565e8fa6f5e6e18d56989477de90ec2e4614d72c
Vasiliy Ulyanov <vulyanov@suse.de>
2022-02-07 06:19:03 +0100

    Add VMI admitter for SEV pre-attestation
    SEV pre-attestation requires that VMI is started paused.
    
    Signed-off-by: Vasiliy Ulyanov <vulyanov@suse.de>

--- pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
+++ pkg/virt-api/webhooks/validating-webhook/admitters/vmi-create-admitter_test.go
@@ -3595,6 +3595,20 @@ var _ = Describe("Validating VMICreate Admitter", func() {
 			causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
 			Expect(causes).To(HaveLen(len(vmi.Spec.Domain.Devices.Interfaces)))
 		})
+
+		It("should accept pre-attestation with start strategy 'Paused'", func() {
+			startStrategy := v1.StartStrategyPaused
+			vmi.Spec.Domain.LaunchSecurity.SEV.PreAttestation = true
+			vmi.Spec.StartStrategy = &startStrategy
+			causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
+			Expect(causes).To(HaveLen(0))
+		})
+
+		It("should reject pre-attestation without start strategy 'Paused'", func() {
+			vmi.Spec.Domain.LaunchSecurity.SEV.PreAttestation = true
+			causes := ValidateVirtualMachineInstanceSpec(k8sfield.NewPath("fake"), &vmi.Spec, config)
+			Expect(causes).To(HaveLen(1))
+		})
 	})
 })
 
